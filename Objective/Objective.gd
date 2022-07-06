1d9297e8eb8aefc8cc069d7a8ee5738a7fc960b6
Josiahscott <jossco27@kingshigh.school.nz>
2022-07-06 13:08:27 +1200

    new objective

--- Objective/Objective.gd
+++ Objective/Objective.gd
@@ -4,23 +4,21 @@ var number
 var x
 var y
 var z
-
+onready var new_location = 
 onready var player = get_tree().get_nodes_in_group("Player")[0]
 onready var new_obj = player.connect("new_objective",self,"change_location")
 
 func _ready():
-	randomize()
-	number = int(randi() % 10) 
-	print(number) #To call a random variable e.g random airfield instead of random place on map once airfield design is done
-	
-	global_transform.origin = Vector3(randi() % 10000, randi() % 10000, randi() % 10000)
+	global_transform.origin = Vector3(1000,1000,1000)
+#	global_transform.origin = Vector3(randi() % 10000, randi() % 10000, randi() % 10000)
 #	rotation.y = rand_range(-2.5, 2.5 * PI)
 #	print(rotation.y)
 
 func change_location():
-	global_transform.origin = Vector3(randi() % 10000, randi() % 10000, randi() % 10000)
+	global_transform.origin = Vector3(new_location)
+#	global_transform.origin = Vector3(randi() % 10000, randi() % 10000, randi() % 10000)
 #	rotation.y = rand_range(0, 360)
 #	print(rotation.y)
 	
-func _physics_process(delta):
-		look_at(player.global_transform.origin,Vector3.UP)
+#func _physics_process(delta):
+#		look_at(player.global_transform.origin,Vector3.UP)
--- World.gd
+++ World.gd
@@ -1,5 +1,10 @@
 extends Spatial
 
-func _process(_delta):
-#	$Camera.look_at($Plane.transform.origin, Vector3.UP)
-	pass
+
+
+func _ready():
+	print("hello")
+	var objectives = get_tree().get_nodes_in_group("Objective")
+	for o in objectives:
+		print(o.global_transform.origin)
+	
