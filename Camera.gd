e82851a591b3cd8fdb44364e2ee37f4f7969977b
Josiahscott <jossco27@kingshigh.school.nz>
2022-08-03 10:57:27 +1200

    fixed camera

--- Camera.gd
+++ Camera.gd
@@ -1,26 +1,18 @@
 extends Camera
 
-export var lerp_speed = 0.075
-onready var target = get_parent().get_node("Player")
-onready var camera_pos = get_tree().get_nodes_in_group("CameraPos")[0]
+export var lerp_speed = 3.0
 
-#export (NodePath) var target_path = null
-#export (Vector3) var offset = Vector3.ZERO
+export (NodePath) var target_path = null
+export (Vector3) var offset = Vector3.ZERO
+var target = null
 
+func _ready():
+	if target_path:
+		target = get_node(target_path)
 
-func _process(delta):
-	$Arrow.look_at(get_node("/root/World/Objective").global_transform.origin,Vector3.UP)
-	global_transform.origin = camera_pos.global_transform.origin
-	rotation = rotation.move_toward(target.rotation,lerp_speed)
-
-#func _ready():
-#	if target_path:
-#		target = get_node(target_path)
-#
-#func _physics_process(delta):
-#	if !target:
-#		return
-#	var target_pos = target.global_transform.translated(offset)
-#	global_transform.origin = global_transform.origin.move_toward(target_pos, lerp_speed)
-#	global_transform = global_transform.interpolate_with(target_pos, lerp_speed * delta)
-#	look_at(target.global_transform.origin, target.transform.basis.y)
+func _physics_process(delta):
+	if !target:
+		return
+	var target_pos = target.global_transform.translated(offset)
+	global_transform = global_transform.interpolate_with(target_pos, lerp_speed * delta)
+	look_at(target.global_transform.origin, target.transform.basis.y)
