2e9cbb5bf5fad47a19f6f6ee58d193dfa7222f46
Josiahscott <jossco27@kingshigh.school.nz>
2022-08-18 09:45:56 +1200

    Locations update

--- Objective/Objective.gd
+++ Objective/Objective.gd
@@ -6,8 +6,8 @@ onready var new_obj = player.connect("new_location", self, "change_location")
 
 func _ready():
 	randomize()
-	global_transform.origin = Vector3(1000,1000,1000) #change this to main airport when ready
-	change_location()
+	global_transform.origin = Vector3(5000,5000,5000) #change this to main airport when ready
+#	change_location()
 	
 func change_location():
 	global_transform.origin = locations[randi()%len(locations)].global_transform.origin
